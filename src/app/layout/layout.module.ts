cf39b0a104d9f020ab92b06f49c5b579c2e21183
Oleksii Kluban <jsweetpirozhok@gmail.com>
2023-04-26 20:26:22 +0300

    feat(layout): add header links

--- src/app/layout/layout.module.ts
+++ src/app/layout/layout.module.ts
@@ -1,9 +1,9 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterModule } from '@angular/router';
-import { LayoutComponent } from './layout/layout.component';
 import { HeaderComponent } from './header/header.component';
-
+import { LayoutComponent } from './layout/layout.component';
 
 
 @NgModule({
@@ -13,7 +13,8 @@ import { HeaderComponent } from './header/header.component';
   ],
   imports: [
     CommonModule,
-    RouterModule
+    RouterModule,
+    MatButtonModule
   ]
 })
 export class LayoutModule { }
