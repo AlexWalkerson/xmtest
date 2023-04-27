fdc9d744c83220ab407630039bc256027ef898d8
Oleksii Kluban <jsweetpirozhok@gmail.com>
2023-04-27 08:20:39 +0300

    feat(app): add appInfiniteScroll photo-list component

--- src/app/components/photo-list/photo-list.component.spec.ts
+++ src/app/components/photo-list/photo-list.component.spec.ts
@@ -1,23 +1,113 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { jest } from '@jest/globals';
+import { Subject } from 'rxjs';
+import { PhotoService } from '../../core/services/photo.service';
 
 import { PhotoListComponent } from './photo-list.component';
 
-describe('PhotoListComponent', () => {
+describe('HomeComponent', () => {
   let component: PhotoListComponent;
   let fixture: ComponentFixture<PhotoListComponent>;
+  let photoServiceMock: Partial<PhotoService>;
+  let getTeaserPhotoListSubject: Subject<any>;
+  let getTeaserPhotoSrcSubject: Subject<any>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ PhotoListComponent ]
-    })
-    .compileComponents();
+    getTeaserPhotoListSubject = new Subject();
+    getTeaserPhotoSrcSubject = new Subject();
 
+    photoServiceMock = {
+      getTeaserPhotoList: jest.fn(() => getTeaserPhotoListSubject.asObservable()),
+      getTeaserPhotoSrc: jest.fn(() => getTeaserPhotoSrcSubject.asObservable()),
+    };
+
+    await TestBed.configureTestingModule({
+      imports: [MatProgressSpinnerModule],
+      declarations: [PhotoListComponent],
+      providers: [{provide: PhotoService, useValue: photoServiceMock}],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(PhotoListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  afterEach(() => {
+    getTeaserPhotoListSubject.complete();
+    getTeaserPhotoSrcSubject.complete();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should load more images on init', () => {
+    getTeaserPhotoListSubject.next([1, 2, 3]);
+    getTeaserPhotoSrcSubject.next(['src1', 'src2', 'src3']);
+
+    expect(component.srcList).toEqual(['src1', 'src2', 'src3']);
+    expect(photoServiceMock.getTeaserPhotoList).toHaveBeenCalled();
+    expect(photoServiceMock.getTeaserPhotoSrc).toHaveBeenCalledWith([0, 1, 2]);
+  });
+
+  it('should load more images on loadMore', () => {
+    component['loaded'] = false;
+    component['loadMoreSbj'].next(null);
+
+    getTeaserPhotoListSubject.next([1, 2, 3]);
+    getTeaserPhotoSrcSubject.next(['src1', 'src2', 'src3']);
+
+    expect(component.srcList).toEqual([  "src1", "src2", "src3", "src1", "src2", "src3"]);
+    expect(photoServiceMock.getTeaserPhotoList).toHaveBeenCalled();
+  });
+
+  describe('getBatchIds', () => {
+    it('should return an array of numbers starting from lastLoadedImageIndex and containing up to loadImagesNumber items when lastIndex is greater than lastLoadedImageIndex + loadImagesNumber', () => {
+      component['lastLoadedImageIndex'] = 3;
+      component['lastIndex'] = 10;
+      component['loadImagesNumber'] = 5;
+      const expectedIds = [3, 4, 5, 6, 7];
+
+      const result = component.getBatchIds();
+
+      expect(result).toEqual(expectedIds);
+    });
+
+    it('should return an array of numbers starting from lastLoadedImageIndex and containing up to lastIndex - lastLoadedImageIndex + 1 items when lastIndex is less than or equal to lastLoadedImageIndex + loadImagesNumber', () => {
+      component['lastLoadedImageIndex'] = 3;
+      component['lastIndex'] = 6;
+      component['loadImagesNumber'] = 5;
+      const expectedIds = [3, 4, 5, 6];
+
+      const result = component.getBatchIds();
+
+      expect(result).toEqual(expectedIds);
+    });
+
+    it('should set loaded to true when lastIndex is less than or equal to lastLoadedImageIndex + loadImagesNumber', () => {
+      component['lastLoadedImageIndex'] = 3;
+      component['lastIndex'] = 6;
+      component['loadImagesNumber'] = 5;
+
+      component.getBatchIds();
+
+      expect(component['loaded']).toBe(true);
+    });
+  });
+
+  describe('createIndexRange', () => {
+    it('should return an array of numbers starting from start and containing count items', () => {
+      const start = 5;
+      const count = 3;
+      const expectedIds = [5, 6, 7];
+
+      const result = component.createIndexRange(start, count);
+
+      expect(result).toEqual(expectedIds);
+    });
+  });
+
 });
--- src/app/components/photo-list/photo-list.component.ts
+++ src/app/components/photo-list/photo-list.component.ts
@@ -1,24 +1,77 @@
-import { Component, OnInit } from '@angular/core';
-import { PhotoService } from 'src/app/core/services/photo.service';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { PhotoService } from '../../core/services/photo.service';
+import { InfiniteScrollDirective } from '../../share/directives/infinite-scroll.directive';
+import { mergeMap, Subject, Subscription, tap } from 'rxjs';
 
 @Component({
   selector: 'app-photo-list',
   templateUrl: './photo-list.component.html',
-  styleUrls: ['./photo-list.component.scss']
+  styleUrls: ['./photo-list.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class PhotoListComponent implements OnInit {
+export class PhotoListComponent implements OnInit, OnDestroy {
 
-   srcList: string[] = [];
+  @ViewChild(InfiniteScrollDirective) infiniteScrollDirective!: InfiniteScrollDirective;
 
-  constructor(private readonly photoService: PhotoService) {}
+  loading = false;
+  srcList: string[] = [];
+
+  private loadImagesNumber = 8;
+  private lastLoadedImageIndex: number = 0;
+  private lastIndex: number;
+  private loaded = false;
+  private photoSubscriber: Subscription;
+  private loadMoreSbj: Subject<unknown> = new Subject();
+
+  constructor(
+    private readonly photoService: PhotoService,
+    private cd: ChangeDetectorRef
+  ) {}
 
   ngOnInit() {
-
-    this.photoService.getTeaserPhotoList().pipe(
+    this.photoSubscriber = this.loadMoreSbj.pipe(
+      tap(e => this.loading = true),
+      mergeMap(e => this.photoService.getTeaserPhotoList()),
+      mergeMap(list => {
+        this.lastIndex = list.length - 1;
+        let ids = this.getBatchIds();
+        return this.photoService.getTeaserPhotoSrc(ids);
+      })
     ).subscribe(e => {
-      this.srcList = e;
+      this.srcList = [...this.srcList, ...e];
+      this.loading = false;
+      this.cd.detectChanges();
+      this.infiniteScrollDirective.onScroll(null);
     });
 
+    this.loadMore();
+  }
+
+  ngOnDestroy() {
+    this.photoSubscriber.unsubscribe();
+  }
+
+  loadMore() {
+    if ( !this.loaded) {
+      this.loadMoreSbj.next(null);
+    }
+  }
+
+  getBatchIds(): number[] {
+    let ids: number[];
+    if (this.lastIndex < ( this.lastLoadedImageIndex + this.loadImagesNumber )) {
+      ids = this.createIndexRange(this.lastLoadedImageIndex, ( ( this.lastIndex - this.lastLoadedImageIndex ) + 1 ));
+      this.loaded = true;
+    } else {
+      ids = this.createIndexRange(this.lastLoadedImageIndex, this.loadImagesNumber);
+    }
+    this.lastLoadedImageIndex = ids[ids.length - 1] + 1;
+
+    return ids;
+  }
+
+  createIndexRange(start: number, count: number): number[] {
+    return Array.from({length: count}, (_, i) => start + i);
   }
 
 }
