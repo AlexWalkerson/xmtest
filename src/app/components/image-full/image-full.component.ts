f2a877d23798c944c4797a52722efe01bac415a5
Oleksii Kluban <jsweetpirozhok@gmail.com>
2023-04-27 10:19:41 +0300

    feat(app): implement ImageFullComponent

--- src/app/components/image-full/image-full.component.ts
+++ src/app/components/image-full/image-full.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, combineLatest, Subscription, switchMap } from 'rxjs';
+import { FavoriteService } from 'src/app/core/services/favorite.service';
+
 
 @Component({
   selector: 'app-image-full',
   templateUrl: './image-full.component.html',
-  styleUrls: ['./image-full.component.scss']
+  styleUrls: ['./image-full.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class ImageFullComponent {
+export class ImageFullComponent implements OnInit, OnDestroy {
+  id: number;
+  src: string;
+  private favoriteSb: Subscription;
+  private reload: BehaviorSubject<unknown> = new BehaviorSubject(null);
 
+  constructor(
+    private route: ActivatedRoute,
+    private readonly favoriteService: FavoriteService,
+    private readonly cd: ChangeDetectorRef
+  ) { }
+
+  ngOnInit() {
+    this.favoriteSb = combineLatest([this.route.params, this.reload]).pipe(
+      switchMap(([params, e]) => {
+        this.id = params['id'];
+        return this.favoriteService.getFavoriteSrcById(this.id);
+      })
+    ).subscribe(src => {
+      this.src = src;
+      this.cd.detectChanges();
+    });
+  }
+
+  remove() {
+    this.favoriteService.remove(this.id);
+    this.reload.next(null);
+  }
+
+  ngOnDestroy(): void {
+    this.favoriteSb.unsubscribe();
+  }
 }
